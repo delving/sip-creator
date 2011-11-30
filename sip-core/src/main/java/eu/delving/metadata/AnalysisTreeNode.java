delving/sip-creator

commit 1bd93754626f220d41b27fb1355051fef6491d60

Analysis tree now showing record counts on each node

sip-core/src/main/java/eu/delving/metadata/AnalysisTreeNode.java
@@ -24,12 +24,7 @@ package eu.delving.metadata;
 import javax.swing.tree.TreeNode;
 import javax.swing.tree.TreePath;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Vector;
+import java.util.*;
 
 /**
  * A node of the analysis tree
@@ -222,6 +217,9 @@ public class AnalysisTreeNode implements AnalysisTree.Node, Serializable {
     public String toString() {
         if (tag == null) {
             return "?";
+        }
+        else if (fieldStatistics != null) {
+            return String.format("%s (%d)", tag.toString(), fieldStatistics.getTotal());
         }
         else {
             return tag.toString();

commit cf1d5a6563dac10bea35b545fc5a07570e313311

Fixed code generation bug introduced recently in 1bd9375462

sip-core/src/main/java/eu/delving/metadata/AnalysisTreeNode.java
@@ -153,7 +153,7 @@ public class AnalysisTreeNode implements AnalysisTree.Node, Serializable {
         StringBuilder out = new StringBuilder("input.");
         Iterator<AnalysisTreeNode> nodeWalk = path.iterator();
         while (nodeWalk.hasNext()) {
-            String nodeName = nodeWalk.next().toString();
+            String nodeName = nodeWalk.next().tag.toString();
             out.append(SourceVariable.Filter.tagToVariable(nodeName));
             if (nodeWalk.hasNext()) {
                 out.append('.');