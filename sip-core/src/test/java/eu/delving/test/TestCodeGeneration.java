56f9bf60cb5ac41fad88397de237eb47a0dbb282
Gerald de Jong <gerald@delving.eu>
2013-04-10 11:26:08 +0200

    Refactored TestCodeGeneration to get everything from resources.

--- sip-core/src/test/java/eu/delving/test/TestCodeGeneration.java
+++ sip-core/src/test/java/eu/delving/test/TestCodeGeneration.java
@@ -25,10 +25,14 @@ import eu.delving.groovy.*;
 import eu.delving.metadata.*;
 import eu.delving.schema.SchemaVersion;
 import junit.framework.Assert;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
 import org.junit.Test;
 import org.w3c.dom.Node;
 
+import javax.xml.stream.XMLStreamException;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Map;
@@ -41,162 +45,51 @@ import java.util.TreeMap;
  */
 
 public class TestCodeGeneration {
+    private RecDefModel recDefModel = recDefModel();
+    private RecMapping recMapping;
+    private String input, expect;
 
-    static RecMapping recMapping;
-
-    static {
-        try {
-            recMapping = RecMapping.create(recDefModel().createRecDefTree(new SchemaVersion("test", "0.0.0")));
-            recMapping.getRecDefTree().setListener(new ChattyListener());
-        }
-        catch (MetadataException e) {
-            throw new RuntimeException("Problem!");
-        }
+    @Before
+    public void prep() throws IOException, MetadataException {
+        recDefModel.createRecDefTree(new SchemaVersion("test", "0.0.0"));
+        URL mappingResource = getClass().getResource("/codegen/TestCodeGeneration-mapping.xml");
+        recMapping = RecMapping.read(new File(mappingResource.getFile()), recDefModel);
+        recMapping.getRecDefTree().setListener(new ChattyListener());
+        URL inputResource = getClass().getResource("/codegen/TestCodeGeneration-input.xml");
+        input = FileUtils.readFileToString(new File(inputResource.getFile()));
+        URL expectResource = getClass().getResource("/codegen/TestCodeGeneration-expect.xml");
+        expect = FileUtils.readFileToString(new File(expectResource.getFile()));
     }
 
     @Test
-    public void cornucopia() throws MappingException {
-        recMapping.setFact("dogExists", "true");
-
-        node("/test/@sortorder")
-                .addNodeMapping(mapping("/input/leadup/@orderofsort"));
-
-        node("/test/sub-one/events/event")
-                .addNodeMapping(mapping("/input/leadup/record/list/member"));
-
-        NodeMapping eventDict = node("/test/sub-one/events/event/@thevalue")
-                .addNodeMapping(mapping("/input/leadup/record/list/member/concept"));
-
-        eventDict.operator = Operator.ALL;
-        eventDict.dictionary = new TreeMap<String, String>();
-        eventDict.dictionary.put("superhero", "Clay Man");
-        eventDict.dictionary.put("savior", "Jesus!");
-        eventDict.dictionary.put("sidekick", "Clay Horse");
-
-        node("/test/sub-one/events/event/abc-link/@a")
-                .addNodeMapping(mapping("/input/leadup/record/list/member/@index"));
-
-        NodeMapping dictB = node("/test/id")
-                .addNodeMapping(mapping("/input/leadup/@orderofsort"));
-        dictB.dictionary = new TreeMap<String, String>();
-        dictB.dictionary.put("thisaway", "reverse reverse");
-        dictB.dictionary.put("thataway", "forward forward");
-
-        NodeMapping mapping = node("/test/sub-one/events/event/actor")
-                .addNodeMapping(mapping("/input/leadup/record/list/member/name"));
-        mapping.setGroovyCode(
-                "if (_name.contains(' ')) {\n" +
-                        "return _name.split(' ');\n" +
-                        "} else {\n" +
-                        "return _name.text();\n" +
-                        "}"
-        );
-
-        RecDefNode oneTwoTarget = node("/test/sub-one/sub-two");
-        NodeMapping oneTwoMapping = oneTwoTarget.addNodeMapping(mapping("/input/leadup/record/fromOne"));
-        oneTwoMapping.dictionary = new TreeMap<String, String>();
-        oneTwoMapping.dictionary.put("look this up", "forward forward");
-//        RecDefNode abcTarget = node("/test/sub-one/sub-two/abc-link");
-//        abcTarget.addNodeMapping(mapping("/input/leadup/record/fromOne"));
-
-//        oneTwoTarget.addNodeMapping(mapping("/input/leadup/record/fromTwo"));
-
+    public void cornucopia() throws IOException, XMLStreamException, MappingException {
         GroovyCodeResource resource = new GroovyCodeResource(getClass().getClassLoader());
         MappingRunner mappingRunner = new MappingRunner(resource, recMapping, null, true);
         printWithLineNumbers(mappingRunner.getCode());
-        Node node = mappingRunner.runMapping(createInputRecord());
-
+        MetadataRecord inputRecord = createInputRecord();
+        Node node = mappingRunner.runMapping(inputRecord);
         String xml = new XmlSerializer().toXml(node, true);
-        System.out.println(xml);
-        xml = xml.replaceAll("\n[^<]*", "\n").trim();
-        String expect = StringUtils.join(EXPECT, '\n');
         Assert.assertEquals("Unexpected xml", expect, xml);
     }
 
     private void printWithLineNumbers(String code) {
         int lineNumber = 1;
-        for (String line : code.split("\n")) {
-            System.out.println(String.format("%3d: %s", lineNumber++, line));
-        }
+        for (String line : code.split("\n")) System.out.println(String.format("%3d: %s", lineNumber++, line));
     }
 
-    private MetadataRecord createInputRecord() {
-        GroovyNode input, leadup, record, list, member1, member2, fromOne, fromTwo;
-        input = n(null, "input");
-        leadup = n(input, "leadup");
-        leadup.attributes().put("orderofsort", "thisaway");
-        record = n(leadup, "record");
-        list = n(record, "list");
-        member1 = n(list, "member");
-        member1.attributes().put("index", "23");
-        n(member1, "name", "Gumby Dammit");
-        n(member1, "concept", "superhero");
-        n(member1, "concept", "savior");
-        member2 = n(list, "member");
-        member2.attributes().put("index", "45");
-        n(member2, "name", "<![CDATA[<]]>O'Po<![CDATA[k]]>ey<![CDATA[>]]>");
-        n(member2, "name", "McPokey");
-        n(member2, "concept", "sidekick");
-        n(record, "fromOne", "look this up");
-//        n(record, "fromOne", "http://uri.com/but with spaces/whichis[weird\\stupid]/");
-//        n(record, "fromTwo", "2");
-
+    private MetadataRecord createInputRecord() throws IOException, XMLStreamException {
         Map<String, String> ns = new TreeMap<String, String>();
         ns.put("test", "http://testicles.org");
-        return new MetadataRecordFactory(ns).fromGroovyNode(input, -1, 1);
-    }
-
-    private static final String[] EXPECT = {
-            "<?xml version='1.0' encoding='UTF-8'?>",
-            "<test:test xmlns:test=\"http://www.testicles.org\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" test:sortorder=\"thisaway\" xsi:schemaLocation=\"http://www.testicles.org http://www.testicles.org/hang.xsd\">",
-            "<test:id>thisaway</test:id>",
-            "<test:sub-one>",
-            "<test:sub-two>",
-            "<test:abc-link test:a=\"fw\">forward forward</test:abc-link>",
-            "</test:sub-two>",
-            "<test:events>",
-            "<test:event test:thekey=\"man\" test:theschema=\"http://gumby-and-pokey.com/definitions\" test:thevalue=\"Clay Man\">",
-            "<test:abc-link test:a=\"23\"/>",
-            "<test:actor>Gumby</test:actor>",
-            "<test:actor>Dammit</test:actor>",
-            "</test:event>",
-            "<test:event test:thekey=\"horse\" test:theschema=\"http://gumby-and-pokey.com/definitions\" test:thevalue=\"Clay Horse\">",
-            "<test:abc-link test:a=\"45\"/>",
-            "<test:actor><![CDATA[<]]>O'Po<![CDATA[k]]>ey<![CDATA[>]]></test:actor>",
-            "<test:actor>McPokey</test:actor>",
-            "</test:event>",
-            "</test:events>",
-            "</test:sub-one>",
-            "</test:test>",
-    };
-
-    private static GroovyNode n(GroovyNode parent, String name) {
-        return new GroovyNode(parent, name);
-    }
-
-    private static GroovyNode n(GroovyNode parent, String name, String value) {
-        return new GroovyNode(parent, name, value);
-    }
-
-    private static NodeMapping mapping(String path) {
-        return new NodeMapping().setInputPath(Path.create(path));
-    }
-
-    private static RecDefNode node(String pathString) {
-        Path path = Path.create(pathString).withDefaultPrefix("test");
-        RecDefNode node = recMapping.getRecDefTree().getRecDefNode(path);
-        Assert.assertNotNull("Unable to find node: " + path, node);
-        return node;
+        return new MetadataRecordFactory(ns).metadataRecordFrom("ideee", input);
     }
 
     private static RecDefModel recDefModel() {
         return new RecDefModel() {
-
             @Override
             public RecDefTree createRecDefTree(SchemaVersion schemaVersion) {
                 if (!"test".equals(schemaVersion.getPrefix())) throw new RuntimeException();
                 try {
-                    URL url = TestCodeGeneration.class.getResource("/codegen/test-code-generation-recdef.xml");
+                    URL url = TestCodeGeneration.class.getResource("/codegen/TestCodeGeneration-recdef.xml");
                     InputStream inputStream = url.openStream();
                     return RecDefTree.create(RecDef.read(inputStream));
                 }
