58df1d00be5015c4dadaffe87e3ef7dd94a35abd
Gerald de Jong <gerald@delving.eu>
2013-06-02 09:47:22 +0200

    Updated copyright date

--- sip-app/src/test/java/eu/delving/rdf/MapToCRM.java
+++ sip-app/src/test/java/eu/delving/rdf/MapToCRM.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011, 2012 Delving BV
+ * Copyright 2013 Delving BV
  *
  *  Licensed under the EUPL, Version 1.0 or? as soon they
  *  will be approved by the European Commission - subsequent
