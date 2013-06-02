65821e0951fca18ce1dcb735b67b58cf0f4b23a9
Gerald de Jong <gerald@delving.eu>
2013-06-02 09:51:19 +0200

    Commented out the currently irrelevant stuff

--- sip-app/src/test/java/eu/delving/rdf/MapToCRM.java
+++ sip-app/src/test/java/eu/delving/rdf/MapToCRM.java
@@ -28,19 +28,15 @@ import com.thoughtworks.xstream.converters.reflection.PureJavaReflectionProvider
 import com.thoughtworks.xstream.io.naming.NoNameCoder;
 import com.thoughtworks.xstream.io.xml.XppDriver;
 import eu.delving.XMLToolFactory;
-import eu.delving.sip.URIPolicies;
-import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.namespace.NamespaceContext;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.*;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Method;
 import java.util.*;
 
 /**
@@ -49,12 +45,10 @@ import java.util.*;
 
 public class MapToCRM {
     private static Logger LOG = Logger.getLogger(MapToCRM.class);
-    private static final URIPolicies POLICIES = new URIPolicies();
     private static final XPathFactory PATH_FACTORY = XMLToolFactory.xpathFactory();
     private static final NamespaceContext NAMESPACE_CONTEXT = new XPathContext(new String[][]{
             {"lido", "http://www.lido-schema.org"}
     });
-    private static final String UNUSED_CLASS_NAME = "UNUSED_CLASS_NAME";
 
     public static Mappings readForthMapping(InputStream inputStream) throws IOException, ParserConfigurationException, SAXException {
         return (Mappings) stream().fromXML(inputStream);
@@ -183,117 +177,6 @@ public class MapToCRM {
 
         @XStreamOmitField
         private List<String> argList = new ArrayList<String>();
-
-        public String apply(Node node, String className) {
-            try {
-                if ("Appellation".equals(name)) {
-                    // appellationURI(String className, String subjUri, String appellation)
-                    argList.add(className);
-//                    argList.add(domainMapResult.uri);
-                    fetchArgs(node);
-                }
-                else if ("createLiteral".equals(name)) {
-                    // createLiteral(String className, String type, String note)
-                    argList.add(UNUSED_CLASS_NAME);
-                    argList.add(node.getNodeName());
-                    fetchArgs(node);
-                }
-                else if ("dimensionURI".equals(name)) {
-                    // dimensionURI(String className, String subjUri, String dimensions)
-                    argList.add(UNUSED_CLASS_NAME);
-//                    argList.add(domainMapResult.uri);
-                    fetchArgs(node);
-                }
-                else if ("uriConceptual".equals(name)) {
-                    // uriConceptual(String className, String thing)
-                    argList.add(className);
-                    fetchArgs(node);
-                }
-                else if ("uriEvents".equals(name)) {
-                    // uriEvents(String className, String authority, String eventID, String subjUri)
-                    argList.add(className);
-//                    argList.add(domainMapResult.uri);
-                    fetchArgs(node);
-                }
-                else if ("uriForActors".equals(name)) {
-                    // uriForActors(String className, String authority, String id, String name, String birthDate)
-                    argList.add(className);
-                    fetchArgs(node);
-                }
-                else if ("PhysicalObject".equals(name)) {
-                    // uriForPhysicalObjects(String className, String nameOfMuseum, String entry)
-                    argList.add(UNUSED_CLASS_NAME);
-                    fetchArgs(node);
-                }
-                else if ("Place".equals(name)) {
-                    // uriForPlaces(String className, String placeName, String authority, String placeID,
-                    //              Stribng coordinates, String spaces)
-                    argList.add(UNUSED_CLASS_NAME);
-                    fetchArg(node, 0);
-                    fetchArg(node, 1);
-                    fetchArg(node, 2);
-                    fetchArg(node, 3); // coordinates never really used
-                    argList.add(getPartOfPlaceHack(node));
-                }
-                else if ("PhysicalThing".equals(name)) {
-                    // uriPhysThing(String className, String thing)
-                    argList.add(className);
-                    fetchArgs(node);
-                }
-                else if ("uriTimeSpan".equals(name)) {
-                    // uriTimeSpan(String className, String timespan)
-                    argList.add(UNUSED_CLASS_NAME);
-                    fetchArgs(node);
-                }
-                else if ("Type".equals(name)) {
-                    // uriType(String className, String type)
-                    argList.add(className);
-                    fetchArgs(node);
-                }
-                else {
-                    throw new RuntimeException("Unknown function name: " + name);
-                }
-                Class<?>[] types = new Class<?>[argList.size()];
-                Arrays.fill(types, String.class);
-                try {
-                    Method method = POLICIES.getClass().getMethod(name, types);
-                    return (String) method.invoke(POLICIES, argList.toArray());
-                }
-                catch (NoSuchMethodException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        private void fetchArgs(Node node) {
-            for (Arg a : args) {
-                argList.add(valueAt(node, a.content));
-            }
-        }
-
-        private void fetchArg(Node node, int index) {
-            argList.add(valueAt(node, args.get(index).content));
-        }
-
-        private String getPartOfPlaceHack(Node node) {
-            try { // iterate into partOfPlace fetching names and then join them with dash
-                List<String> places = new ArrayList<String>();
-                while (node != null) {
-                    XPathExpression expr = path().compile("lido:namePlaceSet/lido:appellationValue/text()");
-                    String placeName = (String) expr.evaluate(node, XPathConstants.STRING);
-                    places.add(placeName);
-                    expr = path().compile("lido:partOfPlace");
-                    node = (Node) expr.evaluate(node, XPathConstants.NODE);
-                }
-                return StringUtils.join(places, '-');
-            }
-            catch (XPathExpressionException e) {
-                throw new RuntimeException(e);
-            }
-        }
     }
 
     @XStreamAlias("arg")
@@ -309,27 +192,140 @@ public class MapToCRM {
         }
     }
 
-    private static String valueAt(Node context, String expressionString) {
-        List<Node> nodes = nodeList(context, expressionString);
-        if (nodes.isEmpty()) return "";
-        String value = nodes.get(0).getNodeValue();
-        if (value == null) return "";
-        return value.trim();
-    }
-
-    private static List<Node> nodeList(Node context, String expressionString) {
-        try {
-            XPathExpression expression = path().compile(expressionString);
-            NodeList nodeList = (NodeList) expression.evaluate(context, XPathConstants.NODESET);
-            List<Node> list = new ArrayList<Node>(nodeList.getLength());
-            for (int index = 0; index < nodeList.getLength(); index++) list.add(nodeList.item(index));
-            return list;
-        }
-        catch (XPathExpressionException e) {
-            throw new RuntimeException("XPath Problem", e);
-        }
-    }
-
+// =======================================================================
+// Legacy work-around efforts to study:
+//    public String apply(Node node, String className) {
+//        try {
+//            if ("Appellation".equals(name)) {
+//                // appellationURI(String className, String subjUri, String appellation)
+//                argList.add(className);
+////                    argList.add(domainMapResult.uri);
+//                fetchArgs(node);
+//            }
+//            else if ("createLiteral".equals(name)) {
+//                // createLiteral(String className, String type, String note)
+//                argList.add(UNUSED_CLASS_NAME);
+//                argList.add(node.getNodeName());
+//                fetchArgs(node);
+//            }
+//            else if ("dimensionURI".equals(name)) {
+//                // dimensionURI(String className, String subjUri, String dimensions)
+//                argList.add(UNUSED_CLASS_NAME);
+////                    argList.add(domainMapResult.uri);
+//                fetchArgs(node);
+//            }
+//            else if ("uriConceptual".equals(name)) {
+//                // uriConceptual(String className, String thing)
+//                argList.add(className);
+//                fetchArgs(node);
+//            }
+//            else if ("uriEvents".equals(name)) {
+//                // uriEvents(String className, String authority, String eventID, String subjUri)
+//                argList.add(className);
+////                    argList.add(domainMapResult.uri);
+//                fetchArgs(node);
+//            }
+//            else if ("uriForActors".equals(name)) {
+//                // uriForActors(String className, String authority, String id, String name, String birthDate)
+//                argList.add(className);
+//                fetchArgs(node);
+//            }
+//            else if ("PhysicalObject".equals(name)) {
+//                // uriForPhysicalObjects(String className, String nameOfMuseum, String entry)
+//                argList.add(UNUSED_CLASS_NAME);
+//                fetchArgs(node);
+//            }
+//            else if ("Place".equals(name)) {
+//                // uriForPlaces(String className, String placeName, String authority, String placeID,
+//                //              Stribng coordinates, String spaces)
+//                argList.add(UNUSED_CLASS_NAME);
+//                fetchArg(node, 0);
+//                fetchArg(node, 1);
+//                fetchArg(node, 2);
+//                fetchArg(node, 3); // coordinates never really used
+//                argList.add(getPartOfPlaceHack(node));
+//            }
+//            else if ("PhysicalThing".equals(name)) {
+//                // uriPhysThing(String className, String thing)
+//                argList.add(className);
+//                fetchArgs(node);
+//            }
+//            else if ("uriTimeSpan".equals(name)) {
+//                // uriTimeSpan(String className, String timespan)
+//                argList.add(UNUSED_CLASS_NAME);
+//                fetchArgs(node);
+//            }
+//            else if ("Type".equals(name)) {
+//                // uriType(String className, String type)
+//                argList.add(className);
+//                fetchArgs(node);
+//            }
+//            else {
+//                throw new RuntimeException("Unknown function name: " + name);
+//            }
+//            Class<?>[] types = new Class<?>[argList.size()];
+//            Arrays.fill(types, String.class);
+//            try {
+//                Method method = POLICIES.getClass().getMethod(name, types);
+//                return (String) method.invoke(POLICIES, argList.toArray());
+//            }
+//            catch (NoSuchMethodException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//        catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    private void fetchArgs(Node node) {
+//        for (Arg a : args) {
+//            argList.add(valueAt(node, a.content));
+//        }
+//    }
+//
+//    private void fetchArg(Node node, int index) {
+//        argList.add(valueAt(node, args.get(index).content));
+//    }
+//
+//    private String getPartOfPlaceHack(Node node) {
+//        try { // iterate into partOfPlace fetching names and then join them with dash
+//            List<String> places = new ArrayList<String>();
+//            while (node != null) {
+//                XPathExpression expr = path().compile("lido:namePlaceSet/lido:appellationValue/text()");
+//                String placeName = (String) expr.evaluate(node, XPathConstants.STRING);
+//                places.add(placeName);
+//                expr = path().compile("lido:partOfPlace");
+//                node = (Node) expr.evaluate(node, XPathConstants.NODE);
+//            }
+//            return StringUtils.join(places, '-');
+//        }
+//        catch (XPathExpressionException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    private static String valueAt(Node context, String expressionString) {
+//        List<Node> nodes = nodeList(context, expressionString);
+//        if (nodes.isEmpty()) return "";
+//        String value = nodes.get(0).getNodeValue();
+//        if (value == null) return "";
+//        return value.trim();
+//    }
+//
+//    private static List<Node> nodeList(Node context, String expressionString) {
+//        try {
+//            XPathExpression expression = path().compile(expressionString);
+//            NodeList nodeList = (NodeList) expression.evaluate(context, XPathConstants.NODESET);
+//            List<Node> list = new ArrayList<Node>(nodeList.getLength());
+//            for (int index = 0; index < nodeList.getLength(); index++) list.add(nodeList.item(index));
+//            return list;
+//        }
+//        catch (XPathExpressionException e) {
+//            throw new RuntimeException("XPath Problem", e);
+//        }
+//    }
+//
     private static class XPathContext implements NamespaceContext {
         private Map<String, String> prefixUri = new TreeMap<String, String>();
         private Map<String, String> uriPrefix = new TreeMap<String, String>();
