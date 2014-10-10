6475e5d29e4524d848ebbd2237228ffd574fec7b
geralddejong <gerald@delving.eu>
2014-10-10 15:22:29 +0200

    Adding prefix to minted URL's spec component.

--- sip-app/src/main/java/eu/delving/sip/model/SipModel.java
+++ sip-app/src/main/java/eu/delving/sip/model/SipModel.java
@@ -434,13 +434,11 @@ public class SipModel {
     }
 
     private class Generator implements FileProcessor.UriGenerator {
-        private final String narthexUrl;
         private final String spec;
         private final String prefix;
         private final Matcher matcher;
 
         private Generator(String narthexUrl, String spec, String prefix) {
-            this.narthexUrl = narthexUrl;
             this.spec = spec;
             this.prefix = prefix;
             this.matcher = Pattern.compile("(https?://[^/]+).*").matcher(narthexUrl);
