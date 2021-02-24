3b0b123ba787dc366c12a752885e6d381f6669ad
Ruben <ruben@delving.eu>
2021-02-24 11:20:47 +0100

    Replace SipModel mock with regular instance

--- sip-app/src/test/java/eu/delving/MappingEngineTest.java
+++ sip-app/src/test/java/eu/delving/MappingEngineTest.java
@@ -1,53 +1,46 @@
 package eu.delving;
 
 import eu.delving.groovy.GroovyCodeResource;
+import eu.delving.metadata.CachedResourceResolver;
 import eu.delving.metadata.RecDef;
 import eu.delving.metadata.RecDefTree;
 import eu.delving.metadata.RecMapping;
 import eu.delving.schema.SchemaRepository;
+import eu.delving.sip.Application;
 import eu.delving.sip.base.ProgressListener;
 import eu.delving.sip.files.*;
 import eu.delving.sip.model.Feedback;
 import eu.delving.sip.model.SipModel;
 import eu.delving.sip.model.SipProperties;
-import eu.delving.sip.model.StatsModel;
 import eu.delving.sip.xml.FileProcessor;
 import org.apache.http.client.HttpClient;
 import org.junit.experimental.categories.Category;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.mockito.Mockito;
-import org.mockito.stubbing.Answer;
 import org.testng.Assert;
 import org.xmlunit.builder.Input;
-import org.xmlunit.diff.*;
+import org.xmlunit.diff.DOMDifferenceEngine;
+import org.xmlunit.diff.DifferenceEngine;
 
 import javax.xml.transform.Source;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import static eu.delving.sip.base.HttpClientFactory.createHttpClient;
 import static eu.delving.sip.files.Storage.NARTHEX_URL;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
 
 @RunWith(Parameterized.class)
 @Category(Object.class)
 public class MappingEngineTest {
-
-    private SchemaRepository createSchemaRepository() throws IOException {
-        SipProperties sipProperties = new SipProperties();
-        String serverUrl = sipProperties.getProp().getProperty(NARTHEX_URL, "http://delving.org/narthex");
-        HttpClient httpClient = createHttpClient(serverUrl).build();
-        return new SchemaRepository(new SchemaFetcher(httpClient));
-    }
-
 
     private static Stream<Arguments> testData() {
         return Stream.of(
@@ -62,6 +55,20 @@ public class MappingEngineTest {
                 "naa_0.0.16_record-definition.xml",
                 "naa")
         );
+    }
+
+    private GroovyCodeResource groovyCodeResource;
+
+    @BeforeEach
+    public void before() {
+        groovyCodeResource = new GroovyCodeResource(getClass().getClassLoader());
+    }
+
+    private SchemaRepository createSchemaRepository() throws IOException {
+        SipProperties sipProperties = new SipProperties();
+        String serverUrl = sipProperties.getProp().getProperty(NARTHEX_URL, "http://delving.org/narthex");
+        HttpClient httpClient = createHttpClient(serverUrl).build();
+        return new SchemaRepository(new SchemaFetcher(httpClient));
     }
 
     private Feedback createFeedback() {
@@ -79,25 +86,23 @@ public class MappingEngineTest {
             @Override
             public void alert(String message, Throwable throwable) {
                 System.err.println("alert: " + message);
-                System.err.println(throwable);
+                System.err.println(throwable.getMessage());
+                throwable.printStackTrace(System.err);
             }
 
             @Override
             public String ask(String question) {
-                System.out.println("ask: " + question);
-                return null;
+                throw new IllegalStateException();
             }
 
             @Override
             public String ask(String question, String defaultValue) {
-                System.out.println("ask: question=" + question + ", defaultValue=" + defaultValue);
-                return null;
+                throw new IllegalStateException();
             }
 
             @Override
             public boolean confirm(String title, String message) {
-                System.out.println("confirm: title=" + title + ", message=" + message);
-                return false;
+                throw new IllegalStateException();
             }
 
             @Override
@@ -117,6 +122,27 @@ public class MappingEngineTest {
         };
     }
 
+    private SipModel createSipModel() throws StorageException, IOException {
+        Feedback feedback = createFeedback();
+
+        SipProperties sipProperties = new SipProperties();
+        String serverUrl = sipProperties.getProp().getProperty(NARTHEX_URL, "http://delving.org/narthex");
+        HttpClient httpClient = createHttpClient(serverUrl).build();
+        SchemaRepository schemaRepository = new SchemaRepository(new SchemaFetcher(httpClient));
+        Application.ResolverContext context = new Application.ResolverContext();
+        Storage storage = new StorageImpl(HomeDirectory.WORK_DIR, schemaRepository, new CachedResourceResolver(context));
+        context.setStorage(storage);
+        context.setHttpClient(httpClient);
+
+        return new SipModel(
+            null,
+            storage,
+            groovyCodeResource,
+            feedback,
+            new SipProperties()
+        );
+    }
+
     private InputStream readTestData(String filename) {
         InputStream testData = getClass().getResourceAsStream("/mapped/" + filename);
         assert(testData != null);
@@ -127,63 +153,59 @@ public class MappingEngineTest {
     @ParameterizedTest
     public void test_file_processor(String sipFilename, String mappingFilename, String recDefFilename, String orgId) throws Exception {
         Path workDir = HomeDirectory.WORK_DIR.toPath();
-
-        String sipId = sipFilename.split("__")[0];
-        String rdfFilename = sipId + "__" + orgId + ".xml";
-
-
-        InputStream in = readTestData(rdfFilename);
-
-        Source expectedXml = Input.fromStream(in).build();
-
-        ProgressListener progressListener = mock(ProgressListener.class);
-
-        Feedback feedback = createFeedback();
-
-        StatsModel statsModel = mock(StatsModel.class);
-        when(statsModel.getRecordCount()).thenReturn(0);
-
-        Properties properties = new Properties();
-
-        SipModel sipModel = mock(SipModel.class);
-        when(sipModel.getPreferences()).thenReturn(properties);
-        when(sipModel.getFeedback()).thenReturn(feedback);
-        when(sipModel.getStatsModel()).thenReturn(statsModel);
-
         File sipFile = workDir.resolve(sipFilename).toFile();
         Path mappingFile = sipFile.toPath().resolve(mappingFilename);
-        Path recFile = sipFile.toPath().resolve(recDefFilename);
+        Path recDefFile = sipFile.toPath().resolve(recDefFilename);
 
-        RecDef recDef = RecDef.read(new FileInputStream(recFile.toFile()));
-        RecDefTree recDefTree = RecDefTree.create(recDef);
-        RecMapping mappingDef = RecMapping.read(new FileInputStream(mappingFile.toFile()), recDefTree);
-        DataSet dataSet = new StorageImpl.DataSetImpl(sipFile, createSchemaRepository());
-        dataSet.getDataSetFacts(); // will actually initialize data facts and prevent null pointer later on
-
-
-        GroovyCodeResource groovyCodeResource = new GroovyCodeResource(getClass().getClassLoader());
-        String mappingModelPrefix = "";
         FileProcessor.Listener listener = mock(FileProcessor.Listener.class);
+        ProgressListener progressListener = mock(ProgressListener.class);
+        processSourceXML(sipFile, mappingFile, recDefFile, listener, progressListener);
+
+        String sipPrefix = sipFilename.split("__")[0];
+        String controlXMLFilename = sipPrefix + "__" + orgId + ".xml";
+        InputStream controlXMLStream = readTestData(controlXMLFilename);
+        Path outputXMLFile = workDir.resolve(sipFilename).resolve(controlXMLFilename);
+        Assert.assertTrue(Files.exists(outputXMLFile));
+
+        compareXMLs(controlXMLStream, outputXMLFile);
+    }
+
+    private void processSourceXML(File sipFile,
+                                  Path mappingFile,
+                                  Path recDefFile,
+                                  FileProcessor.Listener listener,
+                                  ProgressListener progressListener) throws IOException, StorageException {
+        SipModel sipModel = createSipModel();
+        RecMapping recMapping = getRecMapping(mappingFile, recDefFile);
+        DataSet sourceXML = new StorageImpl.DataSetImpl(sipFile, createSchemaRepository());
+        sourceXML.getDataSetFacts(); // will actually initialize data facts and prevent null pointer later on
+        FileProcessor.UriGenerator uriGenerator = new SipModel.Generator("http://localhost/narthex/", sourceXML.getSpec(), "");
+
         FileProcessor fileProcessor = new FileProcessor(
             sipModel,
-            dataSet,
-            mappingDef,
+            sourceXML,
+            recMapping,
             false,
             groovyCodeResource,
-            new SipModel.Generator("http://localhost/narthex/", dataSet.getSpec(), mappingModelPrefix),
+            uriGenerator,
             listener
         );
         fileProcessor.setProgressListener(progressListener);
         fileProcessor.run();
+    }
 
-        Path rdfFile = workDir.resolve(sipFilename).resolve(rdfFilename);
-        Assert.assertTrue(Files.exists(rdfFile));
+    private RecMapping getRecMapping(Path mappingFile, Path recFile) throws FileNotFoundException, UnsupportedEncodingException {
+        RecDef recDef = RecDef.read(new FileInputStream(recFile.toFile()));
+        RecDefTree recDefTree = RecDefTree.create(recDef);
+        RecMapping mappingDef = RecMapping.read(new FileInputStream(mappingFile.toFile()), recDefTree);
+        return mappingDef;
+    }
 
-        Source actualXml = Input.fromPath(rdfFile).build();
+    private void compareXMLs(InputStream controlXMLStream, Path outputXMLFile) {
+        Source expectedXml = Input.fromStream(controlXMLStream).build();
+        Source actualXml = Input.fromPath(outputXMLFile).build();
         DifferenceEngine diff = new DOMDifferenceEngine();
-        diff.addDifferenceListener((comparison, comparisonResult) -> {
-            Assert.fail("found a difference: " + comparison);
-        });
+        diff.addDifferenceListener((comparison, comparisonResult) -> Assertions.fail("found a difference: " + comparison));
         diff.compare(expectedXml, actualXml);
     }
 }
